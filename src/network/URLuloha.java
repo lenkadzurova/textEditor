2fbbbd256ccdfbc247260c6d9a31154e50672e60
lenka <jhkjh55@gmail.com>
2020-11-03 22:30:24 +0100

    pokus2 spocitanie slov

--- src/network/URLuloha.java
+++ src/network/URLuloha.java
@@ -12,28 +12,24 @@ public class URLuloha {
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 
             String vystup;
-            int celkovypocet = 0;
             while ((vystup = reader.readLine()) != null) {
                 //System.out.println(vystup);
+                String slovo1 = "div";
+                int slovo1Pocet = 0;
+                int celkovypocet = 0;
 
-                for (int x= 0; x < vystup.length(); x++){
-                    switch (vystup.substring(x)){
-                        case "div":
-                            celkovypocet++;
-                            break;
-                        case "script":
-                            celkovypocet++;
-                            break;
-                    }
+                while (slovo1Pocet != -1) {
+                    slovo1Pocet = vystup.indexOf(slovo1, slovo1Pocet);
+                    if (slovo1Pocet != -1)
+                        celkovypocet++;
+
+                    slovo1Pocet += slovo1.length();
                 }
                 System.out.println(celkovypocet);
             }
             reader.close();
-
-
-
-
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
